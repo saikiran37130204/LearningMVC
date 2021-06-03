32b9160e71e87cf6b85ff06084f32fd7bf5f279e
Kondreddy Saikiran reddy <Saikirankondreddy.1@gmail.com>
2021-06-03 14:41:47 +0530

    first commit

---
+++ LearningMVCProject/Controllers/AuthorController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LearningMVCProject.Models;
+using Microsoft.Extensions.Logging;
+using LearningMVCProject.Services;
+
+namespace LearningMVCProject.Controllers
+{
+    public class AuthorController : Controller
+    {
+        private ILogger<AuthorController> _logger;
+        private IRepo<Author> _repo;
+
+        public AuthorController(IRepo<Author> repo, ILogger<AuthorController> logger)
+        {
+            _repo = repo;
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Author> authors = _repo.GetAll().ToList();
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Author author)
+        {
+            _repo.Add(author);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id)
+        {
+            Author author = _repo.Get(id);
+            return View(author);
+        }
+        [HttpPost]
+        public IActionResult Edit(int id,Author author)
+        {
+            _repo.Update(id, author);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ LearningMVCProject/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using LearningMVCProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+//using System.Web.Mvc.ViewMasterPage;
+namespace LearningMVCProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            try
+            {
+                int num1 = 10, num2 = 0, result = 0;
+                result = num1 / num2;
+            }
+            catch(Exception e)
+            {
+                _logger.LogError("from the index " + e.Message);
+                return RedirectToAction("Error");
+            }
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Services()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
